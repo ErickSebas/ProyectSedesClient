4f5074567316830f3f97e6f6a08a41de8e8fb454
PedroConde20 <cvp2019844@est.univalle.edu>
2023-08-22 08:23:04 -0400

    maps en pantalla cliente y dependencias

--- lib/presentation/screens/HomeClient.dart
+++ lib/presentation/screens/HomeClient.dart
@@ -1,10 +1,20 @@
+import 'package:fluttapp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const HomeClient());
 
+
+
+List<LatLng> polylineCoordinates = [];
 class HomeClient extends StatelessWidget {
   const HomeClient({super.key});
 
+  static const LatLng sourceLocation = LatLng(37.3350926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +26,24 @@ class HomeClient extends StatelessWidget {
           child: Image.asset("assets/cocacola.png"),
         ),
       ),
-      //body: const ListProductsClients(),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: sourceLocation,
+          zoom: 14.5,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId("sourceMarker"),
+            position: sourceLocation,
+            infoWindow: InfoWindow(title: "Origen"),
+          ),
+          const Marker(
+            markerId: MarkerId("destinationMarker"),
+            position: destination,
+            infoWindow: InfoWindow(title: "Destino"),
+          ),
+        },
+      ),
     );
   }
 }
