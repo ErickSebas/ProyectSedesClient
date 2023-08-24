3fdb27a26c037a4bd2caeefacc2f17d58595ef30
ErickSebas <ume0028566@est.univalle.edu>
2023-08-24 15:44:21 -0200

    Erick-Pedro

--- lib/presentation/screens/HomeClient.dart
+++ lib/presentation/screens/HomeClient.dart
@@ -26,7 +26,28 @@ class HomeClient extends StatefulWidget {
 }
 
 class _HomeClientState extends State<HomeClient> {
-LatLng? myPosition;
+late LatLng? myPosition;
+late LatLng _center = const LatLng(0, 0);
+late GoogleMapController mapController;
+bool estaExpandido = false;
+
+void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    mapController.animateCamera(CameraUpdate.newLatLng(_center));
+    mapController.moveCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(_center.latitude, _center.longitude),
+          zoom: 14.5,
+        ),
+      ),
+    );
+  }
+
+  Future<void> Actualizar_Ubicacion() async {
+  LatLng newLocation = LatLng(_center.latitude, _center.longitude);
+  mapController.animateCamera(CameraUpdate.newLatLng(newLocation));
+}
 
   Future<Position> Determinar_Posicion() async {
     LocationPermission permiso;
@@ -42,16 +63,19 @@ LatLng? myPosition;
 
   Obtener_Ubicacion_Actual() async{
     Position posicion = await Determinar_Posicion();
+
     setState(() {
       myPosition = LatLng(posicion.latitude, posicion.longitude);
+      _center = LatLng(posicion.latitude, posicion.longitude);
+      //mapController.animateCamera(CameraUpdate.newLatLng(_center));
     });
-    print(myPosition);
     return posicion;
   }
 
   @override
   void initState(){
-    Obtener_Ubicacion_Actual();
+    Obtener_Ubicacion_Actual(); // que este metodo se ejecute cuando el mapController no sea nullo, usa await o lo q sea
+    //_getLocation();
     super.initState();
   }
 
@@ -77,8 +101,9 @@ LatLng? myPosition;
   ),
 ],
       ),
-      body: myPosition == null ? CircularProgressIndicator(): Column(
+      body:Column(
         children: [
+          
           Expanded(
             child: FutureBuilder(
               future: Obtener_Archivo(),
@@ -98,7 +123,7 @@ LatLng? myPosition;
 
                   return FutureBuilder<List<Marker>>(
                     // Build the list of markers asynchronously
-                    future: _createMarkers(locations),
+                    future: Crear_Puntos(locations),
                     builder: (context, markersSnapshot) {
                       if (markersSnapshot.connectionState == ConnectionState.waiting) {
                         return GoogleMap(
@@ -106,18 +131,75 @@ LatLng? myPosition;
                             target: LatLng(-17.3895000, -66.1568000),
                             zoom: 14.5,
                           ),
+                          onMapCreated: _onMapCreated,
                         );
                       } else if (markersSnapshot.hasError) {
                         return Center(
                           child: Text('Error: ${markersSnapshot.error}'),
                         );
                       } else if (markersSnapshot.hasData) {
-                        return GoogleMap(
+                        return Stack(children: [
+                          GoogleMap(
                           initialCameraPosition: const CameraPosition(
                             target: LatLng(-17.3895000, -66.1568000),
                             zoom: 14.5,
                           ),
                           markers: Set<Marker>.of(markersSnapshot.data!),
+                          onMapCreated: _onMapCreated,
+                        ),
+                        Positioned(
+                            top: 16.0,
+                            left: 16.0,
+                            child: FloatingActionButton(
+                              onPressed: Actualizar_Ubicacion,
+                              tooltip: 'Update Location',
+                              child: Icon(Icons.location_searching),
+                              backgroundColor: Color(0xFF86ABF9), 
+                            ),
+                          ),
+                          Positioned(
+                            top: 16.0,
+                            right: 16.0,
+                            child: Align(
+            //alignment: Alignment.centerRight,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: estaExpandido ? 150 : 60,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  
+                  
+                  if (estaExpandido)
+                    FloatingActionButton(
+                      onPressed: () {
+                        
+                      },
+                      child: Icon(Icons.vaccines_sharp),
+                      backgroundColor: Color(0xFF86ABF9),
+                    ),
+                  if (estaExpandido)
+                    FloatingActionButton(
+                      onPressed: () {
+
+                      },
+                      child: Icon(Icons.facebook_sharp),
+                      backgroundColor: Color(0xFF86ABF9),
+                    ),
+                  if (estaExpandido)
+                    FloatingActionButton(
+                      onPressed: () {
+
+                      },
+                      child: Icon(Icons.tiktok_sharp),
+                      backgroundColor: Color(0xFF86ABF9),
+                    ),
+                ],
+              ),
+            ),
+          ),
+                          ),
+                        ],
                         );
                       } else {
                         return const Center(
@@ -159,7 +241,7 @@ LatLng? myPosition;
     );
   }
 
-  Future<List<Marker>> _createMarkers(List<dynamic>? locations) async {
+  Future<List<Marker>> Crear_Puntos(List<dynamic>? locations) async {
     List<Marker> markers = [];
 
     for (var location in locations!) {
@@ -175,9 +257,21 @@ BitmapDescriptor customIcon = await BitmapDescriptor.fromAssetImage(
           ),
           icon: customIcon,
           infoWindow: InfoWindow(title: location['name']),
+          
         ),
       );
     }
+    BitmapDescriptor customIcon = await BitmapDescriptor.fromAssetImage(
+  ImageConfiguration(size: Size(50, 50)), 'assets/imgUbicacionA.png');
+    markers.add(
+        Marker(
+          markerId: MarkerId("curr_loc"),
+          position: _center,
+          icon: customIcon,
+          infoWindow: InfoWindow(title: 'Tú'),
+        ),
+        
+      );
 
     return markers;
   }
