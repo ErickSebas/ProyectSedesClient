b5700282a86f8429126b85dc56c7675c1d90fbf4
JoseBascope <BTJ0027390@est.univalle.edu>
2023-08-25 14:28:12 -0400

    Jose

--- lib/presentation/screens/HomeClient.dart
+++ lib/presentation/screens/HomeClient.dart
@@ -3,11 +3,10 @@
 /// Nombre del desarrollador: Equipo-Sedes-Univalle
 /// Fecha de creación: 18/08/2023
 /// </summary>
-/// 
+///
 // <copyright file="HomeClient.dart" company="Sedes-Univalle">
 // Esta clase está restringida para su uso, sin la previa autorización de Sedes-Univalle.
 // </copyright>
-
 
 import 'dart:async';
 import 'package:flutter/material.dart';
@@ -26,69 +25,159 @@ class HomeClient extends StatefulWidget {
 }
 
 class _HomeClientState extends State<HomeClient> {
-final Completer<GoogleMapController> _controllerCompleter = Completer<GoogleMapController>();
-List<Marker> markers = [];
-late StreamSubscription<LocationData>? locationSubscription;
-late LatLng? myPosition;
-late LatLng _center = const LatLng(0, 0);
-late GoogleMapController mapController;
-bool estaExpandido = true;
-ValueNotifier<List<Marker>> markersNotifier = ValueNotifier(([]));
-ValueNotifier<LatLng> centerNotifier = ValueNotifier(LatLng(0, 0));
+  final Completer<GoogleMapController> _controllerCompleter =
+      Completer<GoogleMapController>();
+  List<Marker> markers = [];
+  late StreamSubscription<LocationData>? locationSubscription;
+  late LatLng? myPosition;
+  late LatLng _center = const LatLng(0, 0);
+  late GoogleMapController mapController;
+  bool estaExpandido = true;
+  ValueNotifier<List<Marker>> markersNotifier = ValueNotifier(([]));
+  ValueNotifier<LatLng> centerNotifier = ValueNotifier(LatLng(0, 0));
 
-void _onMapCreated(GoogleMapController controller) {
-  mapController = controller;
-  _goToUserLocation();
-}
-
-_launchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    _goToUserLocation();
   }
-}
 
-void Permisos() async{
-  LocationPermission permiso;
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void Permisos() async {
+    LocationPermission permiso;
     permiso = await Geolocator.checkPermission();
-    if(permiso == LocationPermission.denied){
+    if (permiso == LocationPermission.denied) {
       permiso = await Geolocator.requestPermission();
-      if(permiso == LocationPermission.denied){
+      if (permiso == LocationPermission.denied) {
         return Future.error('error');
       }
+    }
   }
-}
 
-Future<void> _goToUserLocation() async {
-  Permisos();
-  final Position position = await Geolocator.getCurrentPosition();
-  mapController.animateCamera(CameraUpdate.newCameraPosition(
-    CameraPosition(
-      target: LatLng(position.latitude, position.longitude),
-      zoom: 14.5,
-    ),
-  ));
-}
-
-
+  Future<void> _goToUserLocation() async {
+    Permisos();
+    final Position position = await Geolocator.getCurrentPosition();
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(position.latitude, position.longitude),
+        zoom: 14.5,
+      ),
+    ));
+  }
 
   Future<Position> Determinar_Posicion() async {
     LocationPermission permiso;
     permiso = await Geolocator.checkPermission();
-    if(permiso == LocationPermission.denied){
+    if (permiso == LocationPermission.denied) {
       permiso = await Geolocator.requestPermission();
-      if(permiso == LocationPermission.denied){
+      if (permiso == LocationPermission.denied) {
         return Future.error('error');
       }
     }
     return await Geolocator.getCurrentPosition();
   }
 
- 
+  Future<void> Mostrar_Confirmacion() async {
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text('MaYpiVaC')),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 10),
+              Image.asset("assets/LogoUnivalle.png", height: 150, width: 150),
+              SizedBox(height: 10),
+              Text(
+                'Responsables de desarrollo',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Card(
+                elevation: 4,
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Docente Administrativo',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 5),
+                        Text(' Christian Montaño Salvatierra'),
+                        Text('cmontanosa@univalle.edu'),
+                      ],
+                    )),
+              ),
+              Card(
+                elevation: 4,
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Estudiantes:',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 5),
+                        Text('Erick Urquiza Mendoza'),
+                        Text('Fabian Mendez Mejia'),
+                        Text('Pedro Conde Valdez'),
+                        Text('Jose Bascope Tejada'),
+                      ],
+                    )),
+              ),
+              SizedBox(height: 10),
+              Text('©Univalle-MaYpiVaC 2023', style: TextStyle(fontSize: 16)),
+              Text('Todos los derechos reservados',
+                  style: TextStyle(fontSize: 16)),
+            ],
+          ),
+          actions: <Widget>[
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                color: Color(0xFF86ABF9),
+                child: Center(
+                  child: SizedBox(
+                    width: 80, // Adjust width as needed
+                    height: 40, // Adjust height as needed
+                    child: TextButton(
+                      child: Text(
+                        'Cerrar',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     Permisos();
     //_goToUserLocation();
@@ -100,11 +189,10 @@ Future<void> _goToUserLocation() async {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text('MaYpiVaC'),
         backgroundColor: Color(0xFF86ABF9),
         centerTitle: true,
@@ -113,18 +201,29 @@ Future<void> _goToUserLocation() async {
           child: Image.asset("assets/LogoSedes.png"),
         ),
         actions: [
-  IconButton(
-    icon: Icon(Icons.close),
-    onPressed: () {
-      // Cierra la aplicación por completo
-      SystemNavigator.pop();
-    },
-  ),
-],
+          IconButton(
+            icon: Image.asset(
+              "assets/LogoUnivalle2.png",
+              height: 50,
+              width: 50,
+            ),
+            onPressed: () {
+              // Informacion acerca de los desarrolladores
+              Mostrar_Confirmacion();
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              // Cierra la aplicación por completo
+
+              SystemNavigator.pop();
+            },
+          ),
+        ],
       ),
-      body:Column(
+      body: Column(
         children: [
-          
           Expanded(
             child: FutureBuilder(
               future: Obtener_Archivo(),
@@ -132,7 +231,8 @@ Future<void> _goToUserLocation() async {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(
                     child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF86ABF9)),
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(Color(0xFF86ABF9)),
                     ),
                   );
                 } else if (snapshot.hasError) {
@@ -141,11 +241,12 @@ Future<void> _goToUserLocation() async {
                   );
                 } else if (snapshot.hasData) {
                   List? locations = snapshot.data;
-                      return FutureBuilder<List<Marker>>(
+                  return FutureBuilder<List<Marker>>(
                     // Build the list of markers asynchronously
                     future: Crear_Puntos(locations),
                     builder: (context, markersSnapshot) {
-                      if (markersSnapshot.connectionState == ConnectionState.waiting) {
+                      if (markersSnapshot.connectionState ==
+                          ConnectionState.waiting) {
                         return GoogleMap(
                           initialCameraPosition: const CameraPosition(
                             target: LatLng(-17.3895000, -66.1568000),
@@ -158,56 +259,60 @@ Future<void> _goToUserLocation() async {
                           child: Text('Error: ${markersSnapshot.error}'),
                         );
                       } else if (markersSnapshot.hasData) {
-                        return Stack(children: [
-                          GoogleMap(
-                            myLocationEnabled: true,
-                            key: ValueKey("key"),
-                          initialCameraPosition: const CameraPosition(
-                            target: LatLng(-17.3895000, -66.1568000),
-                            zoom: 14.5,
-                          ),
-                          markers: Set<Marker>.of(markers),
-                          onMapCreated: _onMapCreated,
-                          minMaxZoomPreference: MinMaxZoomPreference(12,18),
-                        ),
-                          Positioned(
-                            bottom: 16.0,
-                            left: 16.0,
-                            child: Align(
-            //alignment: Alignment.centerRight,
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              //width: estaExpandido ? 150 : 60,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  if (estaExpandido)
-                    FloatingActionButton(
-                      onPressed: () {
-                        _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
-                        Text("Esta entrando al tiktok");
-                      },
-                      child: Icon(Icons.tiktok_rounded),
-                      backgroundColor: Color.fromRGBO(58,164,64,1),
-                    ),
-                  if (estaExpandido)
-
-                    SizedBox(height: 10),
-                    FloatingActionButton(
-                      onPressed: () {
-                        _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
-                        Text("Esta entrando al tiktok 2");
-                      },
-                      child: Icon(Icons.tiktok_sharp),
-                      backgroundColor: Color.fromRGBO(58,164,64,1),
-                      //backgroundColor: Color.fromRGBO(251,234,3,1),
-                    ),
-                ],
-              ),
-            ),
-          ),
-                          ),
-                        ],
+                        return Stack(
+                          children: [
+                            GoogleMap(
+                              myLocationEnabled: true,
+                              key: ValueKey("key"),
+                              initialCameraPosition: const CameraPosition(
+                                target: LatLng(-17.3895000, -66.1568000),
+                                zoom: 14.5,
+                              ),
+                              markers: Set<Marker>.of(markers),
+                              onMapCreated: _onMapCreated,
+                              minMaxZoomPreference:
+                                  MinMaxZoomPreference(12, 18),
+                            ),
+                            Positioned(
+                              bottom: 16.0,
+                              left: 16.0,
+                              child: Align(
+                                //alignment: Alignment.centerRight,
+                                child: AnimatedContainer(
+                                  duration: Duration(milliseconds: 300),
+                                  //width: estaExpandido ? 150 : 60,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      if (estaExpandido)
+                                        FloatingActionButton(
+                                          onPressed: () {
+                                            _launchURL(
+                                                "https://vm.tiktok.com/ZMjeVX9LC/");
+                                            Text("Esta entrando al tiktok");
+                                          },
+                                          child: Icon(Icons.tiktok_rounded),
+                                          backgroundColor:
+                                              Color.fromRGBO(58, 164, 64, 1),
+                                        ),
+                                      if (estaExpandido) SizedBox(height: 10),
+                                      FloatingActionButton(
+                                        onPressed: () {
+                                          _launchURL(
+                                              "https://vm.tiktok.com/ZMjeVX9LC/");
+                                          Text("Esta entrando al tiktok 2");
+                                        },
+                                        child: Icon(Icons.tiktok_sharp),
+                                        backgroundColor:
+                                            Color.fromRGBO(58, 164, 64, 1),
+                                        //backgroundColor: Color.fromRGBO(251,234,3,1),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         );
                       } else {
                         return const Center(
@@ -216,10 +321,6 @@ Future<void> _goToUserLocation() async {
                       }
                     },
                   );
-
-                  
-                  
-                  
                 } else {
                   return const Center(
                     child: Text('No hay datos disponibles.'),
@@ -228,38 +329,38 @@ Future<void> _goToUserLocation() async {
               },
             ),
           ),
-    Align(
-  alignment: Alignment.bottomCenter,
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Expanded(
-        child: GestureDetector(
-          onTap: () {
-          _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
-          Text("Esta entrando a la gobernacion");
-          },
-          child: Image.asset(
-            "assets/LogoOficialVectorizado.png",
-            fit: BoxFit.contain,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
+                      Text("Esta entrando a la gobernacion");
+                    },
+                    child: Image.asset(
+                      "assets/LogoOficialVectorizado.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
+                      Text("Esta entrando a Departamento");
+                    },
+                    child: Image.asset(
+                      "assets/MarcaDepartamental.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-      Expanded(
-        child: GestureDetector(
-          onTap: () {
-          _launchURL("https://vm.tiktok.com/ZMjeVX9LC/");
-          Text("Esta entrando a Departamento");
-          },
-          child: Image.asset(
-            "assets/MarcaDepartamental.png",
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
-    ],
-  ),
-),
         ],
       ),
     );
@@ -267,8 +368,8 @@ Future<void> _goToUserLocation() async {
 
   Future<List<Marker>> Crear_Puntos(List<dynamic>? locations) async {
     for (var location in locations!) {
-    BitmapDescriptor customIcon = await BitmapDescriptor.fromAssetImage(
-    ImageConfiguration(size: Size(100, 100)), 'assets/Way.png');
+      BitmapDescriptor customIcon = await BitmapDescriptor.fromAssetImage(
+          ImageConfiguration(size: Size(100, 100)), 'assets/Way.png');
       markers.add(
         Marker(
           markerId: MarkerId(location['name']),
@@ -278,7 +379,6 @@ Future<void> _goToUserLocation() async {
           ),
           icon: customIcon,
           infoWindow: InfoWindow(title: location['name']),
-          
         ),
       );
     }
